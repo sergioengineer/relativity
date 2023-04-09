0b2ac4f0b8704cad4b4e2ce97c1ac7ac668ead7f
Sergio C <sergiobg.engineer@gmail.com>
2023-04-09 16:49:46 -0300

    feature: takes surroundinglines configuration into consideration when shifting the visible range

--- src/shiftVisibleRange.ts
+++ src/shiftVisibleRange.ts
@@ -10,9 +10,17 @@ function shiftVisibleRange(
   editor: vscode.TextEditor,
   newPosition: vscode.Position
 ) {
+  
+
+  const config = vscode.workspace.getConfiguration();
+  const surroundingLines = config.get("editor.cursorSurroundingLines");
+
+  const newPositionAfterViewport = editor.visibleRanges[0].end.line <= (newPosition.line + Number(surroundingLines));
+  const newPositionBeforeViewport = editor.visibleRanges[0].start.line >= (newPosition.line + Number(surroundingLines));
+
   // If the new position is out of the editor's top and bottom visible range
   // Move the visible range to show the new position at the center of the editor
-  if (!editor.visibleRanges[0].contains(newPosition)) {
+  if (newPositionAfterViewport || newPositionBeforeViewport ) {
     // Create a new range that is a singular position based on newPosition/jump-destination
     const newRange: vscode.Range = new vscode.Range(newPosition, newPosition);
     // Move the editor's visible range to the newRange and scroll jump destination to center of editor
