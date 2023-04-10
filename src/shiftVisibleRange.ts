82a7297f7225643ec00f852baf1f88e2fbe34d33
Sergio C <sergiobg.engineer@gmail.com>
2023-04-10 05:30:08 -0300

    prettier

--- src/shiftVisibleRange.ts
+++ src/shiftVisibleRange.ts
@@ -10,17 +10,20 @@ function shiftVisibleRange(
   editor: vscode.TextEditor,
   newPosition: vscode.Position
 ) {
-  
-
   const config = vscode.workspace.getConfiguration();
   const surroundingLines = config.get("editor.cursorSurroundingLines");
 
-  const newPositionAfterViewport = editor.visibleRanges[0].end.line <= (newPosition.line + Number(surroundingLines));
-  const newPositionBeforeViewport = editor.visibleRanges[0].start.line >= (newPosition.line + Number(surroundingLines));
+  const newPositionAfterViewport =
+    editor.visibleRanges[0].end.line <=
+    newPosition.line + Number(surroundingLines);
+
+  const newPositionBeforeViewport =
+    editor.visibleRanges[0].start.line >=
+    newPosition.line + Number(surroundingLines);
 
   // If the new position is out of the editor's top and bottom visible range
   // Move the visible range to show the new position at the center of the editor
-  if (newPositionAfterViewport || newPositionBeforeViewport ) {
+  if (newPositionAfterViewport || newPositionBeforeViewport) {
     // Create a new range that is a singular position based on newPosition/jump-destination
     const newRange: vscode.Range = new vscode.Range(newPosition, newPosition);
     // Move the editor's visible range to the newRange and scroll jump destination to center of editor
